3fe03fe4179495eb22b728b973c97ed51504c08e
William Vulcano <99343816+williamdubgod@users.noreply.github.com>
2022-08-23 22:56:52 -0300

    Add files via upload

---
+++ ex01.java
@@ -0,0 +1,24 @@
+package entrada_processamento_saida;
+
+import java.util.Scanner;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		double base, altura, area;
+		
+		System.out.printf("Digite a base do retangulo: ");
+		base = ler.nextDouble();
+				
+		System.out.printf("Digite a altura do retangulo: ");
+		altura = ler.nextDouble();
+		
+		area = base * altura;
+		
+		System.out.printf("A area do retangulo e: %.1f", area);
+
+	}
+
+}
