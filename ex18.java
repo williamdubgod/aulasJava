0569c2282dbdeb92266aaed6fffbfb8a82b27e22
William Vulcano <99343816+williamdubgod@users.noreply.github.com>
2022-08-31 23:09:08 -0300

    Update and rename ex13.java to ex18.java

--- ex18.java
+++ ex18.java
@@ -2,7 +2,7 @@ package entrada_processamento_saida;
 
 import java.util.Scanner;
 
-public class ex13 {
+public class ex18 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
