94cd14f71afd8be630058d357d8f0beaa067c0c3
williamdubgod <william_vulcano@hotmail.com>
2022-09-30 17:02:10 -0300

    exercicio conta bancaria

---
+++ ExercicioContaBancaria/ContaBancaria.java
@@ -0,0 +1,16 @@
+package poo;
+
+public class ContaBancaria {
+	String agencia;
+	String numero;
+	double saldo;
+	
+	ContaBancaria() {
+	}
+	
+	ContaBancaria(String agencia, String numero, double saldo) {
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+}
