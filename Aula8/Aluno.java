f891570d5fa1092e0525e7ce37d3d1557179bbc6
williamdubgod <william_vulcano@hotmail.com>
2022-10-14 23:02:11 -0300

    aula 8 poo

---
+++ Aula8/Aluno.java
@@ -0,0 +1,41 @@
+package poo;
+
+public class Aluno {
+	 private int id;
+	 private String nome;
+	 private String ra;
+	    public int getId() {
+	        return id;
+	    }
+	 
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	 
+	    public String getNome() {
+	        return nome;
+	    }
+	 
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+	 
+	    public String getRa() {
+	        return ra;
+	    }
+	    
+	    public void setRa(String ra) {
+	        this.ra = ra;
+	    }
+	   
+	    Aluno(){
+	       
+	    }
+	   
+	    Aluno(String nome, String ra){
+	        this.nome = nome;
+	        this.ra = ra;
+	    }
+	    
+}
+
